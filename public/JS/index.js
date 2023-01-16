52320d91b0cd96d77027e488fed9717a72e9b3ea
Rohit <patilrohit2513@gmail.com>
2023-01-16 14:33:46 +0530

    minor

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,3 +1,9 @@
 $("#threeLines").click(function(){
     $("#list").toggleClass("flex");
 })
+$("#mobTitle").click(function(){
+    $("#list").removeClass("flex");
+})
+$("#navbar").click(function(){
+    $("#list").removeClass("flex");
+})
